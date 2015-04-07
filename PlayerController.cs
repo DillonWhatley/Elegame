ca6098e95b2a5b8d984066ca49213ecd35f7eb0b
Dillon Whatley <dillmaximus@gmail.com>
2015-04-07 13:50:19 -0400

    april7 class

---
+++ PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Text UItext;
+	int UIint;
+
+	void Start () {
+		UIint = 0;
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive(false);
+			updateText();
+		}
+	}
+
+	void updateText(){
+		UIint ++;
+		UItext.text = "Elebits Collected:"+ UIint.ToString();
+	}
+
+}
---
+++ test2/Assets/EnchantedAnvil/Scripts/AlienCollider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AlienCollider : MonoBehaviour {
+
+	private int eleCount;
+	public GUIText eleCountText;
+	
+	void Start (){
+		eleCount = 0;
+		eleCountText.text = "0";
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive(false);
+			eleCount ++;
+			eleCountText.text = eleCount.ToString();
+		}
+	}
+}
